f859f996cb2de4b8002b95ee04a33be543737aca
Максим Муратов <muratoff.max@yandex.ru>
2023-12-15 18:34:22 +0300

    pep8 fixes 2

--- backend/api/v1/permissions.py
+++ backend/api/v1/permissions.py
@@ -12,13 +12,11 @@ class RecipePermission(BasePermission):
     def has_permission(self, request, view):
         return (
             request.method in SAFE_METHODS
-            or
-            request.user.is_authenticated
+            or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
         return (
             request.method in SAFE_METHODS
-            or
-            (request.user.is_authenticated and obj.author == request.user)
+            or (request.user.is_authenticated and obj.author == request.user)
         )
